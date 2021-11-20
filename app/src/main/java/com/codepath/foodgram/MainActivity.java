e8f604925e86d2f0a808fcd0590f3c049d80c4a9
Nezar Vidad <nezarv2k@gmail.com>
2021-11-20 15:50:14 -0500

    redesigned bottom navigation

--- app/src/main/java/com/codepath/foodgram/MainActivity.java
+++ app/src/main/java/com/codepath/foodgram/MainActivity.java
@@ -10,9 +10,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import com.codepath.foodgram.fragments.ChatFragment;
-import com.codepath.foodgram.fragments.FeedFragment;
+import com.codepath.foodgram.fragments.FriendsFragment;
 import com.codepath.foodgram.fragments.ProfileFragment;
-import com.codepath.foodgram.fragments.StoreFragment;
+import com.codepath.foodgram.fragments.StoresFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.parse.ParseUser;
@@ -30,24 +30,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bottomNavigation = findViewById(R.id.bottomNavigation);
-        Menu menu = bottomNavigation.getMenu();
-        MenuItem feed = menu.findItem(R.id.action_feed);
-        MenuItem profile = menu.findItem(R.id.action_profile);
-        MenuItem store = menu.findItem(R.id.action_store);
 
         bottomNavigation.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 Fragment fragment;
                 switch (item.getItemId()) {
-                    case R.id.action_feed:
-                        fragment = new FeedFragment();
+                    case R.id.action_friends:
+                        fragment = new FriendsFragment();
+                        break;
+                    case R.id.action_stores:
+                        fragment = new StoresFragment();
                         break;
                     case R.id.action_profile:
                         fragment = new ProfileFragment();
-                        break;
-                    case R.id.action_store:
-                        fragment = new StoreFragment();
                         break;
                     default:
                         fragment = new ChatFragment();
@@ -61,18 +57,10 @@ public class MainActivity extends AppCompatActivity {
         ParseUser currentUser = ParseUser.getCurrentUser();
 
         // Set default selection
-        if (currentUser.getString("type").equals("FoodStore")) { // Food store logged in
-            feed.setVisible(false);
-            profile.setVisible(false);
-            store.setVisible(true);
-            bottomNavigation.setSelectedItemId(R.id.action_store);
-        }
-        else { // Normal user logged in
-            feed.setVisible(true);
-            profile.setVisible(true);
-            store.setVisible(false);
-            bottomNavigation.setSelectedItemId(R.id.action_feed);
-        }
+        if (currentUser.getString("type").equals("FoodStore")) // Food store logged in
+            bottomNavigation.setSelectedItemId(R.id.action_profile);
+        else // Normal user logged in
+            bottomNavigation.setSelectedItemId(R.id.action_friends);
 
     }
 
