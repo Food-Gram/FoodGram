7c660abb52bb1f326ef5e3ff3c7545d22abebff6
Ying Yang <yyang005@citymail.cuny.edu>
2021-12-08 19:46:11 -0500

    Final Update

--- app/src/main/java/com/codepath/foodgram/adapters/ChatAdapter.java
+++ app/src/main/java/com/codepath/foodgram/adapters/ChatAdapter.java
@@ -2,6 +2,7 @@ package com.codepath.foodgram.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,12 +21,21 @@ import com.codepath.foodgram.R;
 import com.codepath.foodgram.details.DetailActivity_Chat;
 import com.codepath.foodgram.details.DetailActivity_OtherStoreProd;
 import com.codepath.foodgram.details.DetailActivity_OtherUserProf;
+import com.codepath.foodgram.models.ChatMsg;
+import com.codepath.foodgram.models.FoodStorePost;
+import com.codepath.foodgram.models.Friend;
+import com.codepath.foodgram.models.Post;
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder>{
@@ -84,6 +94,10 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder>{
         private ImageView ivImage_user;
         private TextView tvUsername;
         private ParseFile image;
+        private TextView tvTime;
+        private TextView tvLastMsg;
+
+        private List<ChatMsg> allmsg;
 
 
         public ViewHolder(@NonNull View itemView) {
@@ -91,9 +105,13 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder>{
             container = itemView.findViewById(R.id.Container_friendlist7);
             ivImage_user = itemView.findViewById(R.id.ivFriendIcon);
             tvUsername = itemView.findViewById(R.id.tvFriendName);
+            tvTime = itemView.findViewById(R.id.tvChatTime);
+            tvLastMsg = itemView.findViewById(R.id.chatLastMsg);
         }
 
         public void bind(ParseUser user, int position) throws ParseException {
+
+            allmsg = new ArrayList<>();
 
             //Display information
             tvUsername.setText(user.fetchIfNeeded().getUsername());
@@ -106,6 +124,7 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder>{
                         new RoundedCorners(100)).into(ivImage_user);
             }
 
+            queryTime(user);
 
             //onClick
             container.setOnClickListener(new View.OnClickListener() {
@@ -121,6 +140,62 @@ public class ChatAdapter extends RecyclerView.Adapter<ChatAdapter.ViewHolder>{
 
         }
 
+        private void queryTime(ParseUser user) {
+            ParseQuery<ChatMsg> query1 = ParseQuery.getQuery(ChatMsg.class);
+            query1.whereEqualTo(ChatMsg.KEY_SENDER, ParseUser.getCurrentUser());
+            query1.whereEqualTo(ChatMsg.KEY_RECEIVER, user);
+            query1.addDescendingOrder(ChatMsg.KEY_CREATED);
+            query1.findInBackground(new FindCallback<ChatMsg>() {
+                @Override
+                public void done(List<ChatMsg> msgs, ParseException e) {
+                    if(e != null){
+                        Log.e(TAG, "Issue with getting friends", e);
+                        return;
+                    }
+                    for (ChatMsg i : msgs) {
+                        allmsg.add(i);
+                    }
+                    if(allmsg.size()>0) {
+                        Collections.sort(allmsg, new Comparator<ChatMsg>() {
+                            @Override
+                            public int compare(ChatMsg msg1, ChatMsg msg2) {
+                                return (msg1.getCreatedAt().compareTo(msg2.getCreatedAt()) * -1);
+                            }
+                        });
+                        tvTime.setText(allmsg.get(0).getCreatedAt().toString());
+                        tvLastMsg.setText(allmsg.get(0).getContent());
+                    }
+                }
+            });
+            ParseQuery<ChatMsg> query2 = ParseQuery.getQuery(ChatMsg.class);
+            query2.whereEqualTo(ChatMsg.KEY_SENDER, user);
+            query2.whereEqualTo(ChatMsg.KEY_RECEIVER, ParseUser.getCurrentUser());
+            query2.addDescendingOrder(ChatMsg.KEY_CREATED);
+            query2.findInBackground(new FindCallback<ChatMsg>() {
+                @Override
+                public void done(List<ChatMsg> msgs, ParseException e) {
+                    if(e != null){
+                        Log.e(TAG, "Issue with getting friends", e);
+                        return;
+                    }
+                    for (ChatMsg i : msgs) {
+                        allmsg.add(i);
+                    }
+
+                    if(allmsg.size()>0) {
+                        Collections.sort(allmsg, new Comparator<ChatMsg>() {
+                            @Override
+                            public int compare(ChatMsg msg1, ChatMsg msg2) {
+                                return (msg1.getCreatedAt().compareTo(msg2.getCreatedAt()) * -1);
+                            }
+                        });
+                        tvTime.setText(allmsg.get(0).getCreatedAt().toString());
+                        tvLastMsg.setText(allmsg.get(0).getContent());
+                    }
+                }
+            });
+
+        }
 
     }
 }
